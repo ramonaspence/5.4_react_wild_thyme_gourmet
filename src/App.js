52ba72f18575651fd33f89552ca2faf9e895d2ff
RamonaEJSpence <ramonaejspence@gmail.com>
2020-02-09 11:29:30 -0500

    added a cute background img and made the divs a different color with slight transparency. I also now have a function  that console.logs the price of order. However it adds them as strings so 4+5 returns 45 instead of 9. So I have to make these strings integers to begin with, but right now I am trying to use the .parseInt() method

--- src/App.js
+++ src/App.js
@@ -1,70 +1,68 @@
 import React, {Component} from 'react';
+import './backgroundimg.jpg'
 import './App.css';
 
 //menu component needs to be iterated
 const staticFoodItems = [
-  {type: '', key: 0, name: 'Pimento Cheese Ciabatta', price: '$4', description: 'Freshly baked Ciabatta bread, made with thyme and rosemary, served with pimento cheese', category: 'Appetizers'},
-  {type: '', key: 1, name: 'Chips and Cheese, pub-style', price: '$5', description: 'Fresh public-house-style chips made with Russet Potatos, served with your choice of blueu cheese', category: 'Appetizers'},
-  {type: '', key: 2, name: 'Charcuterie Board', price: '$6', description: 'Our specialty charcuterie board brings you our rosemary Focaccia, an assortment of the highest quality cheeses and meats available. Ask your server for today\'s Charcuterie.', category: 'Appetizers'},
-  {type: '', key: 3, name: 'Smoked Trout and Rissoto', price: '$16', description: 'This hickory-smoked Trout comes with a grilled rissoto cake, topped with whipped Ramp Butter', category: 'entree'},
-  {type: '', key: 4, name: 'Chanterelle Creme Pasta', price: '$12', description: 'Locally harvested Chanterelle\'s make up our house-favorite Creme pasta along with a vegetable medley over Fetticcine', category: 'entree'},
-  {type: '', key: 5, name: 'Mahtzo Ball Soup', price: '$14', description: 'A classic, does it need a description? We make our Mahtzo soup broth each day. For the best Mahtzo balls, we wait until you order to put it all together', category: 'dessert'},
-  {type: '', key: 6, name: 'Strawberry Angel Cake', price: '$5', description: 'A twist on the classic dessert Strawberry Short Cake, but with our favorite Angel Cake', category: 'dessert'},
-  {type: '', key: 7, name: 'BlackBerry Peach Pie', price: '$6', description: 'The name doesn\'t say it all. This blackberry peach pie contains subtle notes of chocolate-mint, honey, and bourbon.', category: 'dessert'},
-  {type: '', key: 8, name: 'Key Lime Pie', price: '$5', description: 'This pie captures the tanginess, sweetness, and creaminess that every Key Lime Pie should have', category: ''}]
+  {type: '', key: 0, name: 'Pimento Cheese Ciabatta', price: '4', description: 'Freshly baked Ciabatta bread, made with thyme and rosemary, served with pimento cheese', category: 'Appetizers'},
+  {type: '', key: 1, name: 'Chips and Cheese, pub-style', price: '5', description: 'Fresh public-house-style chips made with Russet Potatos, served with your choice of blueu cheese', category: 'Appetizers'},
+  {type: '', key: 2, name: 'Charcuterie Board', price: '6', description: 'Our specialty charcuterie board brings you our rosemary Focaccia, an assortment of the highest quality cheeses and meats available. Ask your server for today\'s Charcuterie.', category: 'Appetizers'},
+  {type: '', key: 3, name: 'Smoked Trout and Rissoto', price: '16', description: 'This hickory-smoked Trout comes with a grilled rissoto cake, topped with whipped Ramp Butter', category: 'entree'},
+  {type: '', key: 4, name: 'Chanterelle Creme Pasta', price: '12', description: 'Locally harvested Chanterelle\'s make up our house-favorite Creme pasta along with a vegetable medley over Fetticcine', category: 'entree'},
+  {type: '', key: 5, name: 'Mahtzo Ball Soup', price: '14', description: 'A classic, does it need a description? We make our Mahtzo soup broth each day. For the best Mahtzo balls, we wait until you order to put it all together', category: 'dessert'},
+  {type: '', key: 6, name: 'Strawberry Angel Cake', price: '5', description: 'A twist on the classic dessert Strawberry Short Cake, but with our favorite Angel Cake', category: 'dessert'},
+  {type: '', key: 7, name: 'BlackBerry Peach Pie', price: '6', description: 'The name doesn\'t say it all. This blackberry peach pie contains subtle notes of chocolate-mint, honey, and bourbon.', category: 'dessert'},
+  {type: '', key: 8, name: 'Key Lime Pie', price: '5', description: 'This pie captures the tanginess, sweetness, and creaminess that every Key Lime Pie should have', category: ''}]
 
 class Order extends Component {
   constructor(props) {
-    super(props)
+    super(props);
+
+    this.subTotal = this.subTotal.bind(this);
 
     }
 
-  render() {
+    subTotal(props) {
+      let reduceCallBack = (acc, curr) => (acc + curr);
+      const priceList = [...this.props.order];
+      var prices = priceList.map(el => el.price)
+        .parseInt(prices, 10)
+        .reduce(reduceCallBack,0)
+      console.log(prices);
+
+      }
+
+
+
+render() {
+
     const orderList = this.props.order.map((orderItem, key) => (
-      <React.Fragment>
+
+      <div className='orderItems'>
       <ul>
         <li>{orderItem.name}
         </li>
 
-      <li>{orderItem.price}</li>
+      <li>${orderItem.price}</li>
       </ul>
 
-      </React.Fragment>
-    ))
+
+      </div>
+    ));
+
       return (
-        <div className='order col-md-4 mr-auto'>{orderList}</div>
+        <div className='order col-md-4 mr-auto'>{orderList}
+          <h6>Total: ${this.subTotal()}</h6>
+        </div>
+
     )
   }
 
 }
 
-// class FoodItem extends Component {
-//   render() {
-//     return (
-//
-//     )
-//   }
-// }
-//
-// class FoodList extends Component {
-//   render() {
-//     return (
-//       <FoodItem />
-//     )
-//   }
-// }
-
 
 class FoodList extends Component {
-  constructor(props) {
-    super(props);
 
-  }
-
-  // showFoodItem(foodItems) {
-  //   console.log('selectedFood', foodItems);
-  //   this.props.showOrder(foodItems);
-  // }
 
   render() {
     const test = this.props.foodItems.map((foodItems, key) => (
@@ -75,7 +73,7 @@ class FoodList extends Component {
 
         <span>{foodItems.description}</span>
         {/*// <button className='btn mr-5'  onClick={() => {this.showFoodItem.bind(this, foodItems); {this.props.handleOrder.bind(this, foodItems);}}}><h6>{foodItems.price}</h6></button>*/}
-          <button className='btn mr-5'  onClick={this.props.showFoodItem.bind(this, foodItems)}><h6>{foodItems.price}</h6></button>
+          <button className='btn mr-5'  onClick={this.props.showFoodItem.bind(this, foodItems)}><h6>${foodItems.price}</h6></button>
       </div>
     ))
     return (
@@ -120,7 +118,9 @@ class MenuPage extends Component {
   showOrder(foodItems) {
     const order = [...this.state.order];
     this.state.order.push(foodItems);
+
     this.setState(order);
+    console.log('order', this.state.order);
   }
 
   showFoodItem(foodItems) {
